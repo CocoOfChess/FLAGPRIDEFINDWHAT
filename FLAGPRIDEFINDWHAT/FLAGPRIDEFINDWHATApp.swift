06793ad23750950a6afa67ffba3f8471386fefb6
Cami <lafrenz.camryn@gmail.com>
2023-06-28 13:22:14 -0600

    Initial Commit

---
+++ FLAGPRIDEFINDWHAT/FLAGPRIDEFINDWHATApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLAGPRIDEFINDWHATApp.swift
+//  FLAGPRIDEFINDWHAT
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FLAGPRIDEFINDWHATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
